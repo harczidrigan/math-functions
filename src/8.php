c1cb53e5165206e78bf4b07411a4b1762337860c
harczidrigan <harczidrigan@gmail.com>
2025-03-13 10:37:30 +0000

    Add 8.php

---
+++ src/8.php
@@ -0,0 +1,17 @@
+<?php
+
+function randNumber($min = 0, $max = 1) {
+	return mt_rand($min, $max);
+}
+
+function randString($length = 8) {
+	$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+}
+
+?>
