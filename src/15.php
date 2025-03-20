d2f6fb7ce68e720da0273068546720e880a3e6d7
harczidrigan <harczidrigan@gmail.com>
2025-03-20 12:27:25 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,23 @@
+function add($a, $b) {
+    return $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b != 0) {
+        return $a / $b;
+    } else {
+        return "Division by zero is not supported.";
+    }
+}
+
+function power($base, $exponent) {
+    return pow($base, $exponent);
+}
