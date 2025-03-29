8954991a35aa4dfb6e3398f1110332fb9cd67440
harczidrigan <harczidrigan@gmail.com>
2025-03-29 19:03:55 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,14 @@
+<?php
+// Example PHP code to demonstrate mathematical operations
+echo "The sum of 3 and 5 is: " . 3 + 5; // Output: 8
+
+// Example PHP code to calculate the area of a rectangle
+$length = 10;
+$width = 4;
+area = $length * $width;
+echo "The area of the rectangle with length {$length} and width {$width} is: " . $area;
+
+// Example PHP code to perform logarithmic operations
+$logarithm = log(2, 3);
+echo "The logarithm base 3 of 8 is: " . $logarithm;
+?>
