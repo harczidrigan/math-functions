bb3f0d7d351895f5502cd6a5ebd07546cd8f5664
harczidrigan <harczidrigan@gmail.com>
2025-04-02 08:27:51 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,23 @@
+function add($a, $b) {
+    return $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b != 0) {
+        return $a / $b;
+    } else {
+        throw new Exception("Cannot divide by zero");
+    }
+}
+
+function squareRoot($value) {
+    return sqrt($value);
+}
